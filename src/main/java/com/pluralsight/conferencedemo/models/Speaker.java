127ec3dcea49e5dbfa9387697443943ce86dfff8
Carlos Ernesto Diaz <cediazb@siesa.com>
2022-02-04 16:17:04 -0500

    Speaker entity created, with its attributes

---
+++ src/main/java/com/pluralsight/conferencedemo/models/Speaker.java
@@ -0,0 +1,75 @@
+package com.pluralsight.conferencedemo.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "speakers")
+public class Speaker {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String first_name;
+
+    private String last_name;
+
+    private String title;
+
+    private String company;
+
+    private String bio;
+
+    public Speaker() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+}
