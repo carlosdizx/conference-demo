a04045acce63f2ba7a366cb8a952b336b0c16d8f
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2022-02-07 15:36:53 -0500

    Completed the operations crud for Speaker Entity

---
+++ src/main/java/com/pluralsight/conferencedemo/controllers/SpeakerController.java
@@ -0,0 +1,49 @@
+package com.pluralsight.conferencedemo.controllers;
+
+import com.pluralsight.conferencedemo.models.Speaker;
+import com.pluralsight.conferencedemo.repositories.SpeakerRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/speakers/")
+public class SpeakerController {
+
+    @Autowired
+    private SpeakerRepository repository;
+
+
+    @GetMapping
+    public List<Speaker> findAll() {
+        return repository.findAll();
+    }
+
+    @GetMapping
+    @RequestMapping("{id}")
+    public Speaker get(@PathVariable Long id) {
+        return repository.getById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Speaker create(@RequestBody final Speaker speaker) {
+        return repository.saveAndFlush(speaker);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public Speaker update(@PathVariable Long id, @RequestBody Speaker speaker){
+        Speaker existingSpeaker = repository.getById(id);
+        BeanUtils.copyProperties(speaker,existingSpeaker,"id");
+        return repository.saveAndFlush(speaker);
+    }
+
+}
