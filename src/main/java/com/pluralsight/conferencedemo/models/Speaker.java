7a5e4f773dfcbc04bf9ac1063cc27e246965a655
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2022-02-08 12:18:35 -0500

    depurado el codigo, para probar funcionalidades ya funcionan las operaciones de listar, buscar por id (aunque obsoleta creo) y la de crear un registro, para las dos entidades

--- src/main/java/com/pluralsight/conferencedemo/models/Speaker.java
+++ src/main/java/com/pluralsight/conferencedemo/models/Speaker.java
@@ -1,11 +1,14 @@
 package com.pluralsight.conferencedemo.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
 import java.util.List;
 
 @Entity(name = "speakers")
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
 public class Speaker {
 
     @Id
@@ -23,6 +26,7 @@ public class Speaker {
     private String bio;
 
     @ManyToMany(mappedBy = "speakers")
+    @JsonIgnore
     private List<Session> sessions;
 
     @Lob
